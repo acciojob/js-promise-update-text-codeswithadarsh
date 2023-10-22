e029434cb777a7a2dc201481d2eb21213130d12e
Adarsh Pandey <97337633+codeswithadarsh@users.noreply.github.com>
2023-10-22 18:31:04 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,23 @@
 //your JS code here. If required.
+function Hello(){
+	return new Promise((resolve,reject)=>{
+		setTimeout(()=>{
+			resolve("Hello, world!");
+		},1000);
+	});
+}
+Hello().then(data=>{
+   document.getElementById("output").textContent=data;
+});
+
+/*
+function giveFood() {
+  return new Promise((resolve, reject) => {
+    setTimeout(() => {
+      // resolve("giving food...");
+      reject("no bread ");
+    }, 1000);
+  });
+}
+myPromiseFnc().then((data) => console.log("data", data));
+*/
